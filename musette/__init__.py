94683788ec4b2c473796a58925b4644cbe2f20c0
gmflanagan <cyclebelfast@gmail.com>
2014-10-18 10:39:07 +0100

    bump version

--- musette/__init__.py
+++ musette/__init__.py
@@ -1,7 +1,7 @@
 """os.environ proxy based on django-environ"""
 
 __author__ = 'gmflanagan'
-__version__ = (0, 4, 1)
+__version__ = (0, 4, 2)
 
 from ._environ import environ, Environment, ImproperlyConfigured
 
